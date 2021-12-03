ca8685261617854d05f04025aa36a9e689900939
RESUL TUGAY <resultugay@RESULs-MacBook-Pro.local>
2021-12-03 15:55:20 +0000

    improvement for new critic and creator

--- Gate.py
+++ Gate.py
@@ -11,17 +11,21 @@ class Gate:
         if cls._instance is None:
             logging.info('Creating the GATE object')
             cls._instance = super(Gate, cls).__new__(cls)
+        else:
+            logging.info('GATE object alread exist')
+            return cls._instance
         return cls._instance
 
     def initialize(self,args):
         logging.info('GATE initializing')
-        self.creator = CreatorFactory.get_creator(args.creator,args.dataset)
-        self.critic = CriticFactory.get_critic(args.critic,args.dataset)
+        self.creator = CreatorFactory.get_creator(args)
+        self.critic = CriticFactory.get_critic(args)
         logging.info('GATE initialized')
 
 
     def train(self):
         logging.info('Training Started')
+        self.creator.train()
         #Here we create the EM algorithm
         logging.info('Training Finished')
 
--- critic/CriticFactory.py
+++ critic/CriticFactory.py
@@ -2,9 +2,9 @@ from .DenialConstraintCritic import DenialConstraintCritic
 import logging
 
 class CriticFactory:
-    def get_critic(name,dataset):
-        if name == 'Denial':
-            return DenialConstraintCritic(dataset)
+    def get_critic(args):
+        if args.critic == 'Denial':
+            return DenialConstraintCritic(args.dataset)
         else:
-            logging.error('No such critic name as ' + name)
-            raise ValueError(name)
+            logging.error('No such critic name as ' + args.critic)
+            raise ValueError(args.critic)
